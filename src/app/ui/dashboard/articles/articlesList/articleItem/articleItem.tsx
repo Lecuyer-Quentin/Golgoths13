1cbd0fbd1f98ca2d9d908c32765ce1cfd524b044
Lecuyer-Quentin <quentin.lecuyer@mailfence.com>
2024-01-03 01:22:25 +0100

    item article

---
+++ src/app/ui/dashboard/articles/articlesList/articleItem/articleItem.tsx
@@ -0,0 +1,17 @@
+import { Article } from '../../../../../../../types'
+
+type Props = {
+    article: Article
+    }
+
+export default function ArticleItem({ article }: Props) {
+
+    const { title, description, tags} = article;
+
+  return (
+    <li className='flex flex-row'>
+        <p>{title}</p>
+        <p>{description}</p>
+    </li>
+    )
+}
