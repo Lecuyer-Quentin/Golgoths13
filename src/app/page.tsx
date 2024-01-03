3f3276cbc4b34cf0d8e8d4a5a9cad9cedf8553db
Lecuyer-Quentin <quentin.lecuyer@mailfence.com>
2024-01-03 01:16:14 +0100

    home page

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import SignIn from './ui/auth/signIn/signIn'
+
+export default function Home() {
+  return (
+    <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
+      <SignIn />
+    </main>
+  )
+}
