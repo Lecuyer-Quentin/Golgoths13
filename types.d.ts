4fee7948d1c595eb4c204a18ae9eaa010fdc90b8
Lecuyer-Quentin <quentin.lecuyer@mailfence.com>
2024-01-03 19:39:08 +0100

    type global

---
+++ types.d.ts
@@ -0,0 +1,24 @@
+export type Article = {
+    _id: string;
+    title: string;
+    description: string;
+    content: string;
+    tags: string[];
+    cover: string[];
+    images: string[];
+    createdAt: string;
+    updatedAt: string;
+};
+
+export type User = {
+    _id: string;
+    username: string;
+    name: string;
+    lastName: string;
+    password: string;
+    email: string;
+    role: string;
+    avatar: string;
+    createdAt: string;
+    updatedAt: string;
+};
