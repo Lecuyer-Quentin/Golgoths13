fc34c447f7325c1900bf45b48ea40dafd11a7eb6
Lecuyer-Quentin <quentin.lecuyer@mailfence.com>
2024-01-03 01:13:09 +0100

    Users library

---
+++ src/libs/users.ts
@@ -0,0 +1,52 @@
+import axios from 'axios';
+import { User } from '../../types';
+
+
+export async function getSortedUsers() {
+    try {
+        const res = await axios.get('/api/users');
+        const users = res.data.users;
+        const sortedUsers = users.sort((a: User, b: User) => {
+            return new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime();
+        }
+        );
+        return sortedUsers;
+
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+export async function getUser(id: string) {
+    try {
+        const res = await axios.get(`/api/users/${id}`);
+        const user = res.data.user;
+        return user;
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+export async function deleteUser(id: string) {
+    try {
+        await axios.delete(`/api/users`, { data: { id } });
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+export async function updateUser(formData: FormData) {
+    try {
+        await axios.put('/api/users', formData, {
+            headers: {
+                'Content-Type': 'multipart/form-data'
+            },
+        });
+    }
+    catch (error) {
+        console.log(error);
+    }
+}
+
+
+
