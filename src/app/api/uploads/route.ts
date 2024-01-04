700e2f56d749bf0ca9c2155e22e2e4dbd7be9851
Lecuyer-Quentin <quentin.lecuyer@mailfence.com>
2024-01-04 13:19:56 +0100

    uploads route

---
+++ src/app/api/uploads/route.ts
@@ -0,0 +1,12 @@
+import { connectToDbGridFs } from "@/libs/mongo";
+import { NextResponse } from "next/server";
+
+export async function GET() {
+    const { bucket } = await connectToDbGridFs();
+
+    // Obtenez une liste de tous les fichiers dans le bucket
+    const files = await bucket.find().toArray();
+
+    // Renvoyez une liste de noms de fichiers
+    return new NextResponse(JSON.stringify(files.map(file => file.filename)));
+}
