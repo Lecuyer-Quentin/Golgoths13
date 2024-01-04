113e73bbe4169c7aa11f386e9c7fa5aebb4714e2
Lecuyer-Quentin <quentin.lecuyer@mailfence.com>
2024-01-04 13:01:35 +0100

    mongo upload connect

--- src/libs/mongo.ts
+++ src/libs/mongo.ts
@@ -1,4 +1,10 @@
 import mongoose from 'mongoose';
+import { MongoClient, GridFSBucket } from 'mongodb';
+declare global {
+    var client : MongoClient | null;
+    var bucket : GridFSBucket | null;
+}
+
 const { MONGO_URI } = process.env;
 
 if (!MONGO_URI) {
@@ -8,9 +14,26 @@ if (!MONGO_URI) {
 
 export async function connectToDb(){
     try {
-        await mongoose.connect(process.env.MONGO_URI as string);
+        await mongoose.connect(MONGO_URI as string);
         console.log("MongoDB connected");
     } catch (error) {
         console.log(error);
     }
 }
+
+export async function connectToDbGridFs(){
+
+    if(global.client){
+        return {client: global.client, bucket: global.bucket!};
+    }
+        const client = (global.client = new MongoClient(MONGO_URI! as string, {}));
+        const bucket = (global.bucket = new GridFSBucket(client.db(), {
+            bucketName: 'uploads',
+        }));
+        await global.client.connect();
+        console.log("MongoDB connected");
+        return {client: client!, bucket: bucket!};
+
+}
+
+
