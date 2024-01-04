e3cb37394aed140e20057e8d04b43b936dc7dc77
Lecuyer-Quentin <quentin.lecuyer@mailfence.com>
2024-01-04 13:04:23 +0100

    display components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,14 @@
-import SignIn from './ui/auth/signIn/signIn'
+import Hero from './ui/home/hero/hero'
+import LastNews from './ui/home/lastNews/lastNews'
+import Banner from './ui/home/banner/banner'
 
 export default function Home() {
+
   return (
-    <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
-      <SignIn />
+    <main className="flex flex-col items-center justify-center w-full h-full px-20">
+      <Hero />
+      <Banner />
+      <LastNews />
     </main>
   )
 }
