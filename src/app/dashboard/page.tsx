3991ed82cfd19c9d0aba340c4ac2c875f9fd1bba
Lecuyer-Quentin <quentin.lecuyer@mailfence.com>
2024-01-05 19:06:55 +0100

    modif tailwind

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,11 +1,10 @@
-import React from 'react'
 import UsersPage from './users/page'
 import ArticlesPage from './articles/page'
 import TeamsPage from './teams/page'
 
 export default function DashboardPage() {
   return (
-    <div className="flex w-full h-full">
+    <main className="flex w-full h-full">
       <section className="flex flex-col w-full h-full bg-gray-300">
         <ArticlesPage />
         <UsersPage />
@@ -16,7 +15,6 @@ export default function DashboardPage() {
           Right side
         </p>
       </section>
-      
-    </div>
+    </main>
   )
 }
