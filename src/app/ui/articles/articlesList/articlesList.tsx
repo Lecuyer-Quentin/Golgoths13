7c8d579989f80c7aab640b22ed5da91ee15bae50
Lecuyer-Quentin <quentin.lecuyer@mailfence.com>
2024-01-06 14:59:58 +0100

    add loading/error

--- src/app/ui/articles/articlesList/articlesList.tsx
+++ src/app/ui/articles/articlesList/articlesList.tsx
@@ -5,21 +5,31 @@ import { getSortedArticlesData } from '@/libs/articles'
 import Table from '../../components/table/table';
 
 export default function ArticlesList() {
-
     const [articlesListData, setArticlesListData] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
+    const [error, setError] = useState(null);
 
     useEffect(() => {
-        const fetchData = async () => {
-            const data = await getSortedArticlesData();
-            setArticlesListData(data);
-        }
-        fetchData();
-    }
-    , []);
+        getSortedArticlesData()
+            .then((res) => {
+                setArticlesListData(res);
+                setIsLoading(false);
+            })
+            .catch((err) => {
+                setError(err);
+                setIsLoading(false);
+            })
+    }, []);
 
-  return (
-    <section>
-        <Table data={articlesListData} />
-    </section>
-  )
+    if (isLoading) return <div>Loading...</div>
+    if (error) return <div>Error...</div>
+
+    const renderTable = () => {
+      return (
+        <section>
+            <Table data={articlesListData} />
+        </section>
+      )
+    }
+  return renderTable();
 }
