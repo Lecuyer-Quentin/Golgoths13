f1a55cdb1b55079046a0187fad47e54b1d180717
Lecuyer-Quentin <quentin.lecuyer@mailfence.com>
2024-01-06 14:56:49 +0100

    modif tailwind

--- src/app/articles/[id]/page.tsx
+++ src/app/articles/[id]/page.tsx
@@ -1,17 +1,18 @@
-'use client'
+//'use client'
 
 import ArticleDetails from '@/app/ui/articles/articleDetails/articleDetails'
-import {useParams} from 'next/navigation'
 
+export async function generateMetadata() {
+  return {
+    title: 'Article',
+    description: 'Article',
+  }
+}
 export default function ArticlePage () {
-    const { id } = useParams()
-    //const articleId = Array.isArray(id) ? id[0] : id;
-
 
   return (
-    <main>
-        <h1>ArticlePage</h1>
-        <ArticleDetails id={id} />
+    <main className='flex flex-col w-full h-full px-20 py-10 min-h-screen'>
+        <ArticleDetails />
     </main>
 
   )
