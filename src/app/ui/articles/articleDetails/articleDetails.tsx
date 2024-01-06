df65e6e48ec312cff18bc41c997190f696c36bb7
Lecuyer-Quentin <quentin.lecuyer@mailfence.com>
2024-01-06 15:01:51 +0100

    article page component

--- src/app/ui/articles/articleDetails/articleDetails.tsx
+++ src/app/ui/articles/articleDetails/articleDetails.tsx
@@ -1,51 +1,82 @@
+'use client'
+
 import { getArticleDataById } from '@/libs/articles';
 import { useState, useEffect } from 'react'
+import Image from 'next/image'
+import { Article } from '../../../../../types';
+import {useParams} from 'next/navigation'
 
 
-type Props = {
-  id: string[] | string
-}
+export default function ArticleDetails() {
+  const { id } = useParams()
 
-export default function ArticleDetails({id}: Props) {
-  const [title, setTitle] = useState<string>('')
-  const [description, setDescription] = useState<string>('')
-  const [content, setContent] = useState<string>('')
-  const [images, setImages] = useState<string[]>([])
-  const [cover, setCover] = useState<string[]>([])
-  const [tags, setTags] = useState<string[]>([])
-  const [createdAt, setCreatedAt] = useState('')
-  const [updatedAt, setUpdatedAt] = useState('')
-
+  const [isLoading, setIsLoading] = useState(true)
+  const [error, setError] = useState(null)
+  const [articleDetails, setArticleDetails] = useState<Article>()
 
   useEffect(() => {
-    const fetchData = async () => {
-      const res = await getArticleDataById(id as string)
-      setTitle(res.title)
-      setDescription(res.description)
-      setContent(res.content)
-      setImages(res.images)
-      setCover(res.cover)
-      setTags(res.tags)
-      setCreatedAt(res.createdAt)
-      setUpdatedAt(res.updatedAt)
-    }
-    fetchData()
+    getArticleDataById(id as string)
+      .then((res) => {
+        setArticleDetails(res)
+        setIsLoading(false)
+      })
+      .catch((err) => {
+        setError(err)
+        setIsLoading(false)
+      })
   }
-  , [id])
-  
-  return (
-    <>
-    <div>ArticleDetail</div>
-    <div>{id}</div>
-    <div>{title}</div>
-    <div>{description}</div>
-    <div>{content}</div>
-    <div>{images}</div>
-    <div>{cover}</div>
-    <div>{tags}</div>
-    <div>{createdAt}</div>
-    <div>{updatedAt}</div>
+  , [ id ])
 
-    </>
+  if (isLoading) return <div>Loading...</div>
+  if (error) return <div>Error...</div>
+
+  const renderArticleCover = (cover: string[], title: string) => {
+    return (
+      <div className='flex flex-col w-full h-full'>
+        <Image src={cover[0]} alt={title} width={200} height={200} />
+      </div>
+    )
+  }
+
+  const renderArticleImages = (images: string[], title: string) => {
+    return (
+      <div className='flex flex-col w-full h-full'>
+        {images && images.map((image) => {
+          return (
+            <div key={image} className='flex flex-col w-full h-full'>
+              <Image src={image} alt={title} width={200} height={200} />
+            </div>
+          )
+        })}
+      </div>
+    )
+  }
+
+  const renderArticleDetails = (articleDetails: Article) => {
+    const { title, description, content, tags, cover, images } = articleDetails
+    return (
+      <article className='flex flex-col w-full h-full'>
+        <div className='flex flex-col w-full h-full'>
+          <h1>{title}</h1>
+          <p>{description}</p>
+          <p>{content}</p>
+          <p>{tags}</p>
+        </div>
+        <div className='flex flex-col w-full h-full'>
+          {articleDetails && renderArticleCover(cover as string[], title as string)}
+        </div>
+        <div className='flex flex-col w-full h-full'>
+          {articleDetails && renderArticleImages(images as string[], title as string)}
+        </div>
+        
+      </article>
+    )
+  }
+
+  return (
+    <div className='flex flex-col w-full h-full'>
+      {articleDetails && renderArticleDetails(articleDetails)}
+    </div>
   )
 }
+
