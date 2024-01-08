42a1fc899e866370b0c003e0b427fe94f0d7413e
Lecuyer-Quentin <quentin.lecuyer@mailfence.com>
2024-01-08 17:13:59 +0100

    modif design

--- src/app/ui/components/card/card.tsx
+++ src/app/ui/components/card/card.tsx
@@ -4,6 +4,7 @@ import Image from "next/image";
 import { useState } from "react";
 import Link from 'next/link';
 import { Article } from '../../../../../types';
+import InfoBtn from "../button/infoBtn";
 
 type Props = {
     data : Article;
@@ -13,32 +14,69 @@ type Props = {
 export default function Card({data} : Props) {
 
   const [hover, setHover] = useState(false);
-  const toggleHover = () => setHover(!hover);
+  const handleMouseEnter = () => setHover(true);
+  const handleMouseLeave = () => setHover(false);
+
   if(!data) return null
-  const { _id, title, description, cover } = data;
+  const { _id, title, cover, tags } = data;
   const URL = `/articles/${_id}`
-  const coverUrl = cover[0]
+
+  const renderArticleCover = (cover: string[], title: string) => {
+    return (
+      <>
+        <Image src={cover[0]} width={360} height={200}
+             alt={title} className="rounded-t-xl h-2/3 object-cover object-center " />
+      </>
+    )
+  }
+
+  const renderArticleTitle = (title: string) => {
+    return (
+      <>
+        <h3 className="font-bold text-lg absolute top-10 left-10">
+          {title}
+        </h3>
+      </>
+    )
+  }
+
+  const renderArticleTags = (tags: string[]) => {
+    return (
+      <div className="absolute bottom-10 right-10">
+        {tags.map((tag) => {
+          return (
+            <span key={tag} className="bg-yellow-400 text-sm font-bold rounded-xl px-2 py-1 m-1">
+              {tag}
+            </span>
+          )
+        })}
+      </div>
+    )
+  }
+
+
+  const renderCard = () => {
+    return (
+      <Link href={URL}>
+        <article onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave} className="w-[22rem] h-[32rem] rounded-t-xl text-black overflow-hidden shadow-xl">
+          {data && renderArticleCover(cover as string[], title as string)}
+          <div className="flex flex-col justify-around items-center h-1/3 py-2 bg-white relative">
+            {data && renderArticleTitle(title as string)}
+            <hr className="w-4/5 border-1 border-yellow-400"/>
+            {hover 
+              ? InfoBtn()
+              : renderArticleTags(tags as string[])
+            }
+          </div>
+        </article>
+      </Link>
+    )
+  }
 
   return (
-
-    <Link href={URL}>
-      <article onMouseEnter={toggleHover} onMouseLeave={toggleHover} className="w-[22rem] h-[32rem] bg-white rounded-xl relative text-black overflow-hidden shadow-xl">
-        <Image src={coverUrl as string} width={360} height={200}
-             alt={title} className="rounded-t-xl h-2/3 object-cover object-center "  />
-
-        <div className="flex flex-col justify-around items-center h-1/3 px-4 py-2">
-          <h3 className="text-center font-bold text-lg">{title}</h3>
-          <p className="text-center text-sm">{description}</p>
-          <button className={`bg-yellow-400 w-1/2 h-8 rounded-xl flex justify-center items-center font-bold transition-all duration-500 
-                              ${hover 
-                                ? 'border-2 border-black'
-                                : 'border-2 border-yellow-400'}`} >
-            + d&apos;infos
-          </button>
-        </div>
-      </article>
-    </Link>
-    
+    <>
+      {data && renderCard()}
+    </>
   )
 }
 
--- src/app/ui/components/card/cardSm.tsx
+++ src/app/ui/components/card/cardSm.tsx
@@ -4,6 +4,7 @@ import Image from "next/image";
 import { useState } from "react";
 import Link from 'next/link';
 import { Article } from '../../../../../types';
+import InfoBtn from "../button/infoBtn";
 
 
 type Props = {
@@ -14,36 +15,73 @@ type Props = {
 export default function CardSm({data} : Props) {
   const [hover, setHover] = useState(false);
 
-  const toggleHover = () => setHover(!hover);
+  const handleMouseEnter = () => setHover(true);
+  const handleMouseLeave = () => setHover(false);
   if(!data) return null;
-  const { _id, title, cover } = data;
+  const { _id, title, cover,description, tags } = data;
   const URL = `/articles/${_id}`
-  const coverUrl = cover[0]
+
+  const renderArticleCover = (cover: string[], title: string) => {
+    return (
+      <>
+        <Image src={cover[0]} alt={title} fill sizes='auto' className='object-cover' />
+      </>
+    )
+  }
+
+  const renderArticleTitle = (title: string) => {
+    return (
+      <>
+        <h3 className={`font-bold text-lg text-center transition-all duration-300 line-clamp-2`}>
+          {title}
+        </h3>
+      </>
+    )
+  }
+
+
+  const renderArticleTags = (tags: string[]) => {
+    return (
+      <div className="absolute bottom-2 right-2">
+        {tags.map((tag) => {
+          return (
+            <span key={tag} className="text-sm font-bold px-1 py-1">
+              {tag}
+            </span>
+          )
+        })}
+      </div>
+    )
+  }
+
+  const renderArticleDescription = (description: string) => {
+    return (
+      <>
+        <p className={`absolute top-4 left-3 text-sm font-bold w-3/6 line-clamp-3 text-black`} >
+          {description}
+        </p>
+      </>
+    )
+  }
+
+
 
 
   return (
 
     <Link href={URL}>
-      <article onMouseEnter={toggleHover} onMouseLeave={toggleHover} className="w-[18rem] h-[22rem] rounded-xl relative text-white overflow-hidden cursor-pointer">
-        <Image src={coverUrl as string} alt={title} fill sizes='auto' className='object-cover' />
-
-        <div className={`group absolute bottom-0 left-0 rounded-b-xl flex flex-col justify-center items-center transition-all duration-300
+      <article onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave} className="w-[18rem] h-[22rem] rounded-xl relative text-white overflow-hidden cursor-pointer">
+        {data && renderArticleCover(cover, title)}
+        <div className={`absolute bottom-0 left-0 rounded-b-xl flex flex-col justify-center items-center transition-all duration-300
                          ${hover 
                             ? "h-1/3 w-full bg-white after:bg-yellow-500 after:rounded-xl after:absolute after:bottom-4 after:left-50 after:w-4/5 after:h-[2px] "
                             : " w-full h-full bg-black bg-opacity-50 rounded-xl"}`}>
-            <h3 className={` font-bold text-lg  transition-all duration-300
-                            ${hover 
-                                ? "text-black w-full relative bottom-6 left-2"
-                                : "text-white relative top-24 left-2 "}`}>
-                                    {title}
-            </h3>
-
-            <button className={`absolute bottom-6 right-7 rounded-xl w-18 h-6 flex justify-center items-center border-2 border-yellow-500 text-yellow-500 px-2 transition-all duration-300
-                            ${hover 
-                                ? "opacity-100 group-hover:bg-yellow-500 group-hover:text-black group-hover:border-yellow-500"
-                                : "opacity-0"}`}>
-                 + d&apos;infos
-            </button>
+            {data && !hover && renderArticleTitle(title)}
+            {data && hover 
+              ? InfoBtn()
+              : renderArticleTags(tags)
+            }
+            {data && hover && renderArticleDescription(description)}
         </div>
       </article>
     </Link>
