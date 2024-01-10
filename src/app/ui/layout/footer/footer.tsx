b03060c2395064156fe5266fab36efb73a699554
Lecuyer-Quentin <quentin.lecuyer@mailfence.com>
2024-01-10 19:46:52 +0100

    modif loop

--- src/app/ui/layout/footer/footer.tsx
+++ src/app/ui/layout/footer/footer.tsx
@@ -6,6 +6,7 @@ import FFBBLogo from '../../../../../public/logos/ffbbLogo.png';
 import Image from 'next/image';
 import Link from 'next/link';
 import { StaticImageData } from 'next/image';
+import { forEach } from '@/app/utils/forEach';
 
 type SocialLink = {
     name: string,
@@ -66,27 +67,42 @@ const footerLink : FooterLink[] = [
 
 
 export default function Footer() {
+
+const renderSocialLinks = () => {
+    return forEach({of: socialLinks, render: (socialLink : SocialLink) => {
+        const {name, link, logo} = socialLink;
+            return (
+                <Link key={name} href={link} target='_blank' rel='noopener noreferrer' className='m-4'>
+                    <Image src={logo} alt="logo" width={30} height={30}/>
+                </Link>
+            )
+        }
+})}
+
+const renderFooterLinks = () => {
+    return forEach({of: footerLink, render: (footerLink : FooterLink) => {
+        const {name, link} = footerLink;
+            return (
+                <li key={name} className="hover:text-yellow-400 hover:scale-110 transform transition duration-500 ease-in-out">
+                    <Link href={link}>
+                        {name}
+                    </Link>
+                </li>
+            )
+        }
+})}
+
   return (
     <footer className="flex-column justify-center align-center w-full primaryColor">
       
         <div className="relative">
             <div className="flex justify-center before:bg-yellow-400 before:h-1 before:absolute before:bottom-20 before:left-0 before:content-[''] before:z-10 before:w-1/4 before:opacity-50 before:rounded-full before:shadow-2xl after:bg-yellow-400 after:h-1 after:absolute after:bottom-20 after:right-0 after:content-[''] after:z-10 after:w-1/4 after:opacity-50 after:rounded-full">
-                {socialLinks.map(({logo, link}, i) => (
-                  <Link key={i} href={link} target='_blank' rel='noopener noreferrer' className='m-4'>
-                    <Image src={logo} alt="logo" width={30} height={30}/>
-                  </Link>
-                ))}
+                {renderSocialLinks()}
             </div>
             
 
             <ul className="flex justify-center gap-8 my-2">
-                {footerLink.map(({name, link}, i) => (
-                    <li key={i} className="hover:text-yellow-400 hover:scale-110 transform transition duration-500 ease-in-out">
-                        <Link href={link}>
-                            {name}
-                        </Link>
-                    </li>
-                ))}
+                {renderFooterLinks()}
             </ul>
 
             <div className="flex-column justify-center align-center text-yellow-400 text-sm">
