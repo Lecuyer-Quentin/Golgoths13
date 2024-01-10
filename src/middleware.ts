5c3feed4e74fe04ca062a2d63c9a28d69fef003e
Lecuyer-Quentin <quentin.lecuyer@mailfence.com>
2024-01-10 19:51:11 +0100

    middleware

---
+++ src/middleware.ts
@@ -0,0 +1,40 @@
+import { withAuth, NextRequestWithAuth } from "next-auth/middleware"
+import { NextResponse } from "next/server"
+
+export default withAuth(
+    // augment the Request object with the user's token
+    function middleware(req: NextRequestWithAuth) {
+        console.log('middleware', req.nextUrl.pathname)
+        console.log('middleware', req.nextauth.token)
+
+        // only admin/editor users can access the dashboard
+        if(req.nextUrl.pathname.startsWith('/page/dashboard')
+            && req.nextauth.token?.role !== 'admin'
+            && req.nextauth.token?.role !== 'editor'
+        ) {
+            return NextResponse.rewrite(
+                new URL ('/page/denied', req.url)
+            )
+        }
+    },
+      
+    {
+        callbacks: {
+            authorized: ({token}) =>  !!token
+        },
+
+    }
+)
+
+// Protected routes
+export const config = {
+    matcher: [
+        '/page/dashboard',
+        '/page/dashboard/articles',
+        '/page/dashboard/users',
+        '/page/dashboard/teams',
+        '/page/dashboard/settings',
+        '/page/dashboard/help',
+
+    ],
+}
