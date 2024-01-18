8f98f743f65e1c7b7c65297984e177270958808c
Lecuyer-Quentin <quentin.lecuyer@mailfence.com>
2024-01-18 16:31:16 +0100

    modif section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,15 +2,36 @@ import Hero from './ui/home/hero/hero'
 import LastNews from './ui/home/lastNews/lastNews'
 import Banner from './ui/home/banner/banner'
 import Cta from './ui/home/cta/cta'
+import Link from 'next/link';
 
 export default function Home() {
+  const URL_NEWS = '/page/news';
+
 
   return (
-    <main className="flex flex-col items-center justify-center w-full h-full min-h-screen px-5 lg:px-20 py-10">
-      <Cta />
-      <Hero />
-      <Banner />
-      <LastNews />
+    <main className="flex flex-col items-center justify-center w-full h-full min-h-screen px-5 md:px-20 lg:px-20 py-10">
+      <section className="w-full h-screen flex flex-col items-center justify-evenly
+                        lg:justify-start lg:items-start lg:px-20 lg:py-20 ">
+        <Cta />
+      </section>
+
+      <section className="w-full relative flex justify-center items-center mb-20">
+        <Hero />
+      </section>
+
+      <section className="w-full h-auto py-10">
+        <Banner />
+      </section>
+
+      <section id='lastNews' className="w-full h-auto py-10 ">
+          <div className="flex items-center justify-between mb-10">
+            <h2 className="text-4xl font-bold">Last News</h2>
+            <Link href={URL_NEWS}>
+              See all
+            </Link>
+          </div>
+        <LastNews />
+      </section>
     </main>
   )
 }
--- src/app/page/news/page.tsx
+++ src/app/page/news/page.tsx
@@ -1,13 +1,39 @@
 import LastArticles from "../../ui/news/lastArticles/lastArticles"
 import LastNews from "../../ui/news/lastNews/lastNews"
 import LastVideo from "../../ui/news/lastVideo/lastVideo"
+import Link from 'next/link';
 
 export default function News() {
+  const URL_VIDEOS = `/page/videos`
+  const URL_ARTICLES = '/page/articles';
+
+
   return (
     <>
+    <section className="w-full">
+          <div className="flex items-center justify-start mx-4 mb-8">
+            <h2 className="text-4xl font-bold">Last News</h2>
+          </div>
       <LastNews />
+    </section>
+    <section className="w-full py-10">
+          <div className="flex items-center justify-between mx-4 mb-8">
+            <h2 className="text-4xl font-bold">Last Video</h2>
+            <Link href={URL_VIDEOS}>
+                View all
+            </Link>
+          </div>
       <LastVideo />
+    </section>
+    <section className="w-full py-10">
+          <div className="flex items-center justify-between mx-4 mb-8">
+            <h2 className="text-4xl font-bold">Last Articles</h2>
+            <Link href={URL_ARTICLES}>
+              View all
+            </Link>
+          </div>
       <LastArticles />
+    </section>
     </>
   )
 }
