da86c1f4d3010c74ed675f6f064fd99d99656575
Lecuyer-Quentin <quentin.lecuyer@mailfence.com>
2024-01-19 14:19:05 +0100

    modif tailwind

--- src/app/ui/error/error.tsx
+++ src/app/ui/error/error.tsx
@@ -1,5 +1,6 @@
 'use client' // Error components must be Client Components
- 
+
+import { Button } from '@nextui-org/react'
 import { useEffect } from 'react'
  
 export default function Error({
@@ -10,21 +11,15 @@ export default function Error({
   reset: () => void | Promise<void>
 }) {
   useEffect(() => {
-    // Log the error to an error reporting service
     console.error(error)
   }, [error])
  
   return (
-    <div>
+    <div className="flex flex-col items-center justify-center">
       <h2>Something went wrong!</h2>
-      <button
-        onClick={
-          // Attempt to recover by trying to re-render the segment
-          () => reset()
-        }
-      >
+      <Button onClick={() => reset()}>
         Try again
-      </button>
+      </Button>
     </div>
   )
 }
